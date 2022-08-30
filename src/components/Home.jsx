4603d6383d7257e32919a17ee32f42c80a4fd5ef
matheus <matheus.loubach@gmail.com>
2022-08-29 22:16:10 -0300

    Upload Mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../components/HomeResponsive.css'
 import Carousel from 'better-react-carousel'
 import Logo from '../assents/fundo.png';
 import imagem3 from '../assents/Capa_jogos/image 3.png';
@@ -94,7 +95,7 @@ const Home = () => {
      </section>
 
      <section className='carros'>
-     <Carousel cols={6} rows={1} gap={6} loop={1000}>
+     <Carousel cols={6} rows={1} gap={2} loop={1000}>
    
      <Carousel.Item> <div className='detalhes_promo'><img width="176px" height="235.05px" src={imagem4} alt='a'/><p className='descricao_promo'>Return... <p className='novoEpic'>{novo}</p></p><span className='promo'>R$ 5,99</span> <labe>R$ 3,99</labe> </div> </Carousel.Item>
       <Carousel.Item><div className='detalhes_promo'><img width="176px" height="235.05px" src={imagem8}/><p className='descricao_promo'>Century <p className='novoEpic'>{novo}</p></p> <span className='promo'>R$ 5,99</span> <labe>R$ 3,99</labe> </div></Carousel.Item>
@@ -200,7 +201,7 @@ const Home = () => {
 <img src={imagem19} alt="jogosNovos"/>
 <div className="fundo_texto">
 <p className='novoEpic'>{novo}</p>
-<p class="subTitulo">Retreat To Enen</p>
+<p class="subTitulo">Retreat</p>
 <p className='promo'>R$ 5,99</p>
 <labe>R$ 3,99</labe>
 </div>
@@ -221,7 +222,7 @@ const Home = () => {
 </section>
 </section>
 
-    <section  id='background2'>
+    <section id='background2'>
       <div>
       <h3>Explore nosso catalogo</h3>
       <p>Pesquise por gênero, características, preço e muito mais para encontrar seu próximo jogo favorito.</p>
@@ -231,8 +232,7 @@ const Home = () => {
       <img src={imagem21} alt="" />
       </div>
     </section>
-
-    <footer className='footer_container'>
+    <section className='footer_container'>
       <div  id='barra' className='footer'>
       <div className='logo_footer'>
       <img src={Logo}></img>
@@ -248,7 +248,7 @@ const Home = () => {
       </div>
       </div>
       <section id='barra' className='dados'>
-      <div>
+      <div >
       <p className='tit_footer'>Recursos</p>
       <ul>
         <li>Apoie-um-Criador</li>
@@ -288,9 +288,11 @@ const Home = () => {
       </div>
       <p className='epicUnrealIcon'><img src={imagem24}/> </p>
       </section>
-      <p>© 2022, Epic Games, Inc. Todos os direitos reservados. Epic, Epic Games, o logotipo da Epic Games, Fortnite, o logotipo do Fortnite, Unreal, Unreal Engine, o logotipo do Unreal Engine, Unreal Tournament e o logotipo do Unreal Tournament são marcas comerciais ou registradas da Epic Games, Inc. nos Estados Unidos da América e em outros lugares. Outras marcas e nomes de produtos são marcas registradas de seus respectivos donos. Transações para além dos EUA são feitas através da Epic Games International, S.à r.l.   </p>
+      </section>
+      <footer className='container_final'>
+      <h4>© 2022, Epic Games, Inc. Todos os direitos reservados. Epic, Epic Games, o logotipo da Epic Games, Fortnite, o logotipo do Fortnite, Unreal, Unreal Engine, o logotipo do Unreal Engine, Unreal Tournament e o logotipo do Unreal Tournament são marcas comerciais ou registradas da Epic Games, Inc. nos Estados Unidos da América e em outros lugares. Outras marcas e nomes de produtos são marcas registradas de seus respectivos donos. Transações para além dos EUA são feitas através da Epic Games International, S.à r.l.   </h4>
       <p>Developed by: <a href="https://www.linkedin.com/in/matheus-loubach/">Matheus Loubach</a> / Redesigner By: <a href="https://www.linkedin.com/in/elias-coelho-287628178/"> Elias Coelho</a></p>
-     </footer>
+      </footer>
 
 </div>
   )
